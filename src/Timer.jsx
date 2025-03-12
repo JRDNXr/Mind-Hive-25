8ed65dc04963d22a73d603606195e2f61a504fb9
Jordan D'Souza <jrdn_dsouza@hotmail.com>
2025-03-12 19:37:21 -0400

    Cleaned up Timer.jsx file

--- src/Timer.jsx
+++ src/Timer.jsx
@@ -5,13 +5,13 @@ export default function Timer({ timer, clickedNumber }) {
     const formatTime = (timeInMs) => {
         const seconds = Math.floor(timeInMs / 1000);
         const milliseconds = timeInMs % 1000;
-        return `${seconds}.${milliseconds.toString().padStart(3, "0")}`; // Format like "2.150"
+        return `${seconds}.${milliseconds.toString().padStart(3, "0")}`;
     };
 
     return (
         <div className="timer">
             <p>Time: {formatTime(timer)} seconds</p>
-            <p>Click Number: {clickedNumber}</p> {/* Display the number the user needs to find */}
+            <p>Click Number: {clickedNumber}</p>
         </div>
     );
 }
