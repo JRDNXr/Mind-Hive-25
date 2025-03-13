ad65abdefbf85cac30e8f3f113c7e6cdaa08fa89
Jordan D'Souza <jrdn_dsouza@hotmail.com>
2025-03-13 12:09:46 -0400

    Added all 3 buttons.

---
+++ src/GameTitles.jsx
@@ -0,0 +1,11 @@
+import './styles/gameTitles.css'
+
+export default function GameTitles() {
+    return (
+        <div className="titles-container">
+            <button className="brain-button">BRAIN</button >
+            <button className="brain-button">SPEED</button>
+            <button className="brain-button">HIDE</button>
+        </div>
+    );
+}
