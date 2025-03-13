4a6787f1973b824cc03414cebd9133a5bc7527e0
Jordan D'Souza <jrdn_dsouza@hotmail.com>
2025-03-13 12:09:16 -0400

    Imported app.css file and GameTitles.jsx file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { useState, useEffect } from "react";
 import Grid from "./GridLayout.jsx";
+import Titles from "./GameTitles.jsx";
+
+import './styles/app.css'
 
 export default function App() {
     return (
-        <>
+        <div className="app-container">
             <Grid />
-        </>
+            <Titles />
+        </div>
     );
 }
